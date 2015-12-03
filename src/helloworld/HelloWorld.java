07b2d412d2878fe5cd674d5a34e4be8659fdf484
limiy1 <>
2015-12-03 11:18:12 +0100

    test commit

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @author liming
+ */
+/**
+ * @param args the command line arguments
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("this is the new line");
+        System.out.println("this is the 2nd line");
+    }
+}
